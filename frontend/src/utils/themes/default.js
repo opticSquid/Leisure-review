67869fea37f51e1143b0fd8a492d156ef7df3ebc
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-06-02 14:05:22 +0530

    custom theme working

---
+++ frontend/src/utils/themes/default.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ecc829",
+    },
+    secondary: {
+      main: "#13e2e8",
+    },
+  },
+});
---
+++ frontend/src/utils/themes/defaultTheme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ecc829",
+    },
+    secondary: {
+      main: "#13e2e8",
+      //   main:"#23bb4e"
+      // #74b88f
+      // #8b95da
+    },
+  },
+});
+
+export default theme;
